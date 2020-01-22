9c19b1e9c0ac746bbb6cfa335f51f249a720ad3d
PetarGajevic <gajevicp0@gmail.com>
2020-01-22 13:52:01 +0100

    Jelena pusti me

--- public/js/work.js
+++ public/js/work.js
@@ -1,40 +1,113 @@
 
 $(document).ready(function(){
+    // Enable and edit Phone Case
     $('#edit-product').click(function() {
+       
         if(document.getElementById('product3').style.display == 'none'){
+            closeCanvas();
             document.getElementById('product3').style.display = 'flex';
         }else{
             document.getElementById('product3').style.display = 'none';
         }
     });
-    $('#enabled-product').click(function(){
+     $('#enabled-product').click(function(){
         if($('#proizvod').hasClass('disabledbutton')){
             $('#proizvod').removeClass('disabledbutton');
+            $( "#proizvod" ).attr( "value" , 0 );
             $('#enabled-product').html('Enabled');
         }else{
             $('#proizvod').addClass('disabledbutton');
+            $( "#proizvod" ).attr( "value", 1 );
             $('#enabled-product').html('Disabled');
         }
+    }); 
+    // Enable and edit T-Shirt
+    $('#edit-product1').click(function() {
+        if(document.getElementById('product4').style.display == 'none'){
+            closeCanvas();
+            document.getElementById('product4').style.display = 'flex';
+        }else{
+            document.getElementById('product4').style.display = 'none';
+        }
     });
-    // canvas on change show product
+    $('#enabled-product1').click(function(){
+        if($('#proizvod1').hasClass('disabledbutton')){
+            $('#proizvod1').removeClass('disabledbutton');
+            $( "#proizvod1" ).attr( "value" , 0 );
+            $('#enabled-product1').html('Enabled');
+        }else{
+            $('#proizvod1').addClass('disabledbutton');
+            $( "#proizvod1" ).attr( "value" , 1 );
+            $('#enabled-product1').html('Disabled');
+        }
+    });
+
+    // Enable and edit Mugs
+    $('#edit-product2').click(function() {
+        if(document.getElementById('product5').style.display == 'none'){
+            closeCanvas();
+            document.getElementById('product5').style.display = 'flex';
+        }else{
+            document.getElementById('product5').style.display = 'none';
+        }
+    });
+    $('#enabled-product2').click(function(){
+        if($('#proizvod2').hasClass('disabledbutton')){
+            $('#proizvod2').removeClass('disabledbutton');
+            $( "#proizvod2" ).attr( "value" , 0 );
+            $('#enabled-product2').html('Enabled');
+        }else{
+            $('#proizvod2').addClass('disabledbutton');
+            $( "#proizvod2" ).attr( "value" , 1 );
+            $('#enabled-product2').html('Disabled');
+        }
+    });
+    // Close other canvas edit options
+    function closeCanvas(){
+        var els = document.getElementsByClassName("img-div");
+        Array.prototype.forEach.call(els, function(el) {
+            el.style.display = 'none';
+            });
+    }
+
+
+    // canvas on change show phone case product
     $('#product3').mouseup(function(){
         var image = document.getElementById("logo-canvas");
         image.src = canvas3.toDataURL();
     });
-
-    $('.color-choose input').on('click', function(){
-        var phonecase = $(this).attr('data-image');
+     // canvas on change show t-shirt product
+    $('#product4').mouseup(function(){
+        var image1 = document.getElementById("logo-canvas1");
+        image1.src = canvas4.toDataURL();
     });
+
+    $('#product5').mouseup(function(){
+        var image2 = document.getElementById("logo-canvas2");
+        image2.src = canvas5.toDataURL();
+    });
+
+    /* $('.color-choose input').on('click', function(){
+        var phonecase = $(this).attr('data-image');
+    }); */
 
     // Change phone case color
     var colorInput = document.querySelector('#color');
     colorInput.addEventListener('input', ()=>{
       $('.background-div').css("background-color", colorInput.value);
     });
-    // Scale control
-  
+    // Change T-Shirt color
+    var colorInput1 = document.querySelector('#color1');
+    colorInput1.addEventListener('input', ()=>{
+      $('.background-div1').css("background-color", colorInput1.value);
+    });
+    // Change Mug color
+    var colorInput2 = document.querySelector('#color2');
+    colorInput2.addEventListener('input', ()=>{
+      $('.background-div2').css("background-color", colorInput2.value);
+    });
 
-
+    
     
 
 
--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -9,7 +9,7 @@
     </form>
 
     
-    <div id="top">
+  {{--   <div id="top">
         <div id="products">
 
            <div class="prod">
@@ -48,14 +48,16 @@
     
     <button id="capture" onclick="doCapture();">Sacuvaj</button>
 
-    </div>
+    </div> --}}
     
-    {{-- <canvas id="viewport"></canvas> --}}
-
+    <canvas id="canvas8" width="500" height="500"></canvas>
+    <button id="repeat1">Repeat</button>
+    <button id="none1">None</button>
 
     <div class="row">
+        <div class="product-column">
         <div class="row-product">
-        <div id="proizvod" class="disabledbutton">
+        <div id="proizvod" class="save-picture disabledbutton" value="1">
             <div class="background-div">
         <img id="logo-canvas" src="/image/<?php if(!empty($image)){echo $image;} ?>">
         <img class="overlay-panel" src="/images/phonecase.png">
@@ -68,6 +70,8 @@
         <div>
         <button id="edit-product">Edit</button>
         <button id="enabled-product">Disabled</button>
+       
+        
         </div>
     </div>
 </div>
@@ -77,12 +81,7 @@
     </div>
     <div class="product-options">
         <div class="color-choose">
-            {{-- <form id="myForm">
-            
-            <label class="container" for="red"><input data-image="red" type="radio" name=radioName id="red" value="red"><span class="checkmark"></span></label>
-            <label class="container" for="blue"><input data-image="blue" type="radio" name=radioName id="blue" value="blue"><span class="checkmark"></span></label>
-            <label class="container" for="black"><input data-image="black" type="radio" name=radioName id="black" value="black"><span class="checkmark"></span></label>
-            </form> --}}
+        
             <div class="container">
                 <div class="output" id="output"></div>
             
@@ -96,15 +95,215 @@
                 </label>
                 <button id="alignVertically">Vertically</button>
                 <button id="alignHorizontally">Horizontally</button>
+                <div class="repeat-option">
+                    <button id="repeat">Repeat</button>
+                    <button id="none">None</button>
+                    <button id="delete">Delete</button>
+                    <button id="add">Add</button>
+                </div>
               </div>
         </div>
     </div>
     </div>
+</div>
+<div class="product-column">
+    <div class="row-product">
+        <div id="proizvod1" class="save-picture disabledbutton" value="1">
+            <div class="background-div1">
+        <img id="logo-canvas1" src="/image/<?php if(!empty($image)){echo $image;} ?>">
+        <img class="overlay-panel" src="/images/t-shirt.png">
+            </div>
+        </div>
+    <div class="preview-info">
+        <span class="preview-name">
+            T-Shirt
+        </span>
+        <div>
+        <button id="edit-product1">Edit</button>
+        <button id="enabled-product1" onclick="">Disabled</button>
+        </div>
     </div>
+</div>
+    <div id="product4" class="img-div" data-value="1" style="display:none">
+        <div id="app4" >
+            <canvas id="c4" width="200" height="300"></canvas>
+        </div>
+        <div class="product-options1">
+            <div class="color-choose">
+            
+                <div class="container">
+                    <div class="output" id="output"></div>
+                
+                    <div class="result-wrp">
+                       <p>Choose a color</p> 
+                      <input type="color" id="color1">
+                    </div>
+                    <label>
+                        <span>Scale:</span> 
+                        <input type="range" id="scale-control1" value="1" min="0.1" max="3" step="0.1">
+                    </label>
+                    <button id="alignVertically1">Vertically</button>
+                    <button id="alignHorizontally1">Horizontally</button>
+                  </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    {{-- Mugs --}}
+    <div class="product-column">
+        <div class="row-product">
+            <div id="proizvod2" class="save-picture disabledbutton" value="1">
+                <div class="background-div2">
+            <img id="logo-canvas2" src="/image/<?php if(!empty($image)){echo $image;} ?>">
+            <img class="overlay-panel" src="/images/mugs.png">
+                </div>
+            </div>
+        <div class="preview-info">
+            <span class="preview-name">
+                T-Shirt
+            </span>
+            <div>
+            <button id="edit-product2">Edit</button>
+            <button id="enabled-product2" onclick="">Disabled</button>
+            </div>
+        </div>
+    </div>
+        <div id="product5" class="img-div" data-value="1" style="display:none">
+            <div id="app5" >
+                <canvas id="c5" width="200" height="300"></canvas>
+            </div>
+            <div class="product-options2">
+                <div class="color-choose">
+                
+                    <div class="container">
+                        <div class="output" id="output"></div>
+                    
+                        <div class="result-wrp">
+                           <p>Choose a color</p> 
+                          <input type="color" id="color2">
+                        </div>
+                        <label>
+                            <span>Scale:</span> 
+                            <input type="range" id="scale-control2" value="0.001" min="0.01" max="1" step="0.1">
+                        </label>
+                        <button id="alignVertically2">Vertically</button>
+                        <button id="alignHorizontally2">Horizontally</button>
+                      </div>
+                </div>
+            </div>
+        </div>
+        </div>
+</div>
+    </div>
+    <div class="save-work">
+        <button id="capture" onclick="doCapture();">Sacuvaj</button>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+`
+<script>
+var canvas8 = new fabric.Canvas('canvas8');
+fabric.Object.prototype.transparentCorners = false;
 
+  var padding = 0;
+
+  fabric.Image.fromURL('/image/<?php if(!empty($image)){echo $image;} ?>', function(img) {
+    img.scaleToWidth(50);
+    var patternSourceCanvas = new fabric.StaticCanvas();
+    patternSourceCanvas.add(img);
+    patternSourceCanvas.renderAll();
+    var pattern = new fabric.Pattern({
+      source: function() {
+        patternSourceCanvas.setDimensions({
+          width: img.getScaledWidth() + padding,
+          height: img.getScaledHeight() + padding
+        });
+        patternSourceCanvas.renderAll();
+        return patternSourceCanvas.getElement();
+      },
+      repeat: 'repeat'
+    }); 
+
+    var rect = new fabric.Rect({
+        width: 250,
+        height: 250,
+        fill: pattern,
+        objectCaching: false
+    });
+
+    canvas8.add(rect);
+    rect.center().setCoords();
+
+
+
+
+
+canvas8.on('object:scaling', function(e) {
+  if (e.target != null) {
+    console.log(e.target);
+    var obj = e.target;
+        var height = obj.height * obj.scaleY;
+        var width = obj.width * obj.scaleX;
+        obj.height = height;
+        obj.width = width;
+        obj.scaleX = 1;
+        obj.scaleY = 1;
+  }
+});
+
+$('#none1').on('click', function(){
+        canvas8.clear(); 
+      /*   canvas8.remove(img);
+        img.scaleToWidth(100);
+        */
+        canvas8.add(img);
+        pattern.repeat = 'no-repeat';
+      canvas.requestRenderAll();
+        canvas8.requestRenderAll(); 
+        console.log("h: " + img.getScaledHeight());
+        console.log("w: " + img.getScaledWidth());
+    });
+
+    $('#repeat1').on('click', function(){
+        canvas8.clear();
+      /*   canvas8.remove(img);
+        img.scaleToWidth(50);
+        canvas8.add(img); */
+        
+    var patternSourceCanvas = new fabric.StaticCanvas();
+    patternSourceCanvas.add(img);
+    patternSourceCanvas.renderAll();
+    var pattern = new fabric.Pattern({
+      source: function() {
+        patternSourceCanvas.setDimensions({
+          width: img.getScaledWidth() + padding,
+          height: img.getScaledHeight() + padding
+        });
+        patternSourceCanvas.renderAll();
+        return patternSourceCanvas.getElement();
+      },
+      repeat: 'repeat'
+    }); 
+
+    var rect = new fabric.Rect({
+        width: 250,
+        height: 250,
+        fill: pattern,
+        objectCaching: false
+    });
+
+    canvas8.add(rect);
+    rect.center().setCoords();
+    });
+    
+});
+    
+
+
+
+
+</script>
 
          
  
@@ -141,36 +340,181 @@ var imgElement = document.getElementById('myImage');
     img.scaleToWidth(200);
     canvas2.add(img);
 });
+</script>
 
+
+
+<script>
+// Canvas for Phone Case
+ var padding = 0;
+ var image1 = document.getElementById("logo-canvas");
 var canvas3 = new fabric.Canvas('c3');
+var context = canvas3.getContext('2d');
 var imgElement = document.getElementById('myImage');
- fabric.Image.fromURL("/images/<?php if(!empty($image)){echo $image;}  ?>", function(img) {
+ fabric.Image.fromURL("/image/<?php if(!empty($image)){echo $image;}  ?>", function(img) {
     img.set({
     
     });
-    img.scaleToHeight(100);
-    img.scaleToWidth(100);
+   // img.scaleToHeight(100);
+    img.scaleToWidth(200);
     canvas3.add(img);
-    // 
-    $('input[type=range]').on('input', function () {
+
+
+  
+    
+    $('#repeat').on('click', function(){
+    canvas3.remove(img);
+    img.set({
+            'top': 0
+        });
+        img.set({
+            'left': 0
+        });
+        canvas3.add(img);
+    canvas3.add(img);
+    canvas3.clear(); 
+    canvas3.requestRenderAll();
+   // img.scaleToHeight(100);
+   // img.scaleToWidth(100);
+   sleep(300).then(() => {
+     //  canvas3.add(img);
+    img.scaleToWidth(100);
+   // img.scaleToHeight(100);
+   //  img.scaleX = 0.1;
+   //  img.scaleY = 0.1;  
+       
+    var patternSourceCanvas = new fabric.StaticCanvas();
+    patternSourceCanvas.add(img);
+    patternSourceCanvas.renderAll();
+    var pattern = new fabric.Pattern({
+      source: function() {
+        patternSourceCanvas.setDimensions({
+          width: img.getScaledWidth() + padding,
+          height: img.getScaledHeight() + padding
+        });
+        patternSourceCanvas.renderAll();
+        return patternSourceCanvas.getElement();
+      },
+      repeat: 'repeat'
+    }); 
+    var rect = new fabric.Rect({
+        width: 5000,
+        height: 5000,
+        fill: pattern,
+        objectCaching: false
+    });
+  
+    
+    canvas3.add(rect);
+    rect.center().setCoords();
+    image1.src = canvas3.toDataURL();
+    canvas3.requestRenderAll();
+   })
+    });
+
+ 
+
+    $('#none').on('click', function(){
+        canvas3.clear(); 
+        img.scaleToWidth(100);
+        canvas3.add(img);
+        image1.src = canvas3.toDataURL();
+        canvas3.requestRenderAll();
+    });
+
+    
+    $('#scale-control').on('input', function () {
       $(this).trigger('change');
       img.scale(parseFloat($(this).val())).setCoords();
       canvas3.requestRenderAll();
   });
   
-  
-  
   $('#alignVertically').on('click', function(){
     img.centerV(); 
-
+    sleep(100).then(() => {
+    img.setCoords();
+    image1.src = canvas3.toDataURL();
+    })
   });
   
   $('#alignHorizontally').on('click', function(){
-
     img.centerH();
+    sleep(100).then(() => {
+    img.setCoords();
+    image1.src = canvas3.toDataURL();
+})
+  });
+});
+</script>
+
+<script>
+// Canvas for T-Shirt
+var canvas4 = new fabric.Canvas('c4');
+ fabric.Image.fromURL("/image/<?php if(!empty($image)){echo $image;}  ?>", function(img) {
+    img.set({
+    
+    });
+    
+    img.scaleToHeight(200);
+    img.scaleToWidth(200);
+    canvas4.add(img);
+    $('#scale-control1').on('input', function () {
+      $(this).trigger('change');
+      img.scale(parseFloat($(this).val())).setCoords();
+      canvas4.requestRenderAll();
+  });
+    var image2 = document.getElementById("logo-canvas1");
+  $('#alignVertically1').on('click', function(){
+    img.centerV(); 
+    sleep(100).then(() => {
+    img.setCoords();
+    image2.src = canvas4.toDataURL();
+    })
   });
   
+  $('#alignHorizontally1').on('click', function(){
+    img.centerH();
+    sleep(100).then(() => {
+    img.setCoords();
+    image2.src = canvas4.toDataURL();
+})
+  });
 });
+
+// Canvas for Mugs
+var canvas5 = new fabric.Canvas('c5');
+ fabric.Image.fromURL("/image/<?php if(!empty($image)){echo $image;}  ?>", function(img) {
+    img.set({
+    
+    });
+    
+    img.scaleToHeight(200);
+    img.scaleToWidth(200);
+    canvas5.add(img);
+    $('#scale-control2').on('input', function () {
+      $(this).trigger('change');
+      img.scale(parseFloat($(this).val())).setCoords();
+      canvas5.requestRenderAll();
+  });
+    var image3 = document.getElementById("logo-canvas2");
+  $('#alignVertically2').on('click', function(){
+    img.centerV(); 
+    sleep(100).then(() => {
+    img.setCoords();
+    image3.src = canvas5.toDataURL();
+    })
+  });
+  
+  $('#alignHorizontally2').on('click', function(){
+    img.centerH();
+    sleep(100).then(() => {
+    img.setCoords();
+    image3.src = canvas5.toDataURL();
+})
+  });
+});
+
+
 
 
 
@@ -199,23 +543,23 @@ const sleep = (milliseconds) => {
 
 
 function doCapture(){
-        canvas.discardActiveObject();
+       /*  canvas.discardActiveObject();
          canvas.requestRenderAll();
         canvas1.discardActiveObject();
         canvas1.requestRenderAll();
         canvas2.discardActiveObject();
-        canvas2.requestRenderAll();
+        canvas2.requestRenderAll(); */
    
-        sleep(500).then(() => {
+        //  sleep(500).then(() => {
 
-    window.scrollTo(0, 0);
+ /*    window.scrollTo(0, 0);
     document.getElementById('c').style.border = "none";
     document.getElementById('c1').style.border = "none";
     document.getElementById('c2').style.border = "none";
-    
-    var els = document.getElementsByClassName("img-div");
+     */
+    var els = document.getElementsByClassName("save-picture");
     Array.prototype.forEach.call(els, function(el) {
-    if( el.getAttribute('data-value')=='0'){
+    if( el.getAttribute('value')=='0'){
         
         html2canvas(el).then(function (canvas){
             
@@ -238,7 +582,7 @@ function doCapture(){
       
     }
 })
-})
+// })
 }  </script>  
 
 @endsection
